39ab1bbb17879c3a51a3076105179659969038f7
kwatsen <kent@watsen.net>
2022-04-28 11:14:47 -0400

    yanglint testing

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -43,27 +43,27 @@ printf "okay.\n"
 
 
 printf "Testing ex-crypto-types-usage.xml..."
-command="yanglint -m -t config -s ../ex-crypto-types-usage\@*.yang ../ietf-crypto-types\@20*.yang ./ietf-origin.yang  ex-crypto-types-usage.xml"
+command="yanglint -t config ./ietf-origin.yang ./ietf-netconf-acm@2012-02-22.yang ../ietf-crypto-types\@20*.yang ../ex-crypto-types-usage\@*.yang ex-crypto-types-usage.xml"
+#command="yanglint -m -t config -s ../ex-crypto-types-usage\@*.yang ../ietf-crypto-types\@20*.yang ./ietf-origin.yang  ex-crypto-types-usage.xml"
 run_unix_cmd $LINENO "$command" 0
 printf "okay.\n"
 
 
 printf "Testing ex-crypto-types-gcsr-rpc.xml..."
-command="yanglint -t rpc ../ex-crypto-types-usage\@*.yang ex-crypto-types-gcsr-rpc.xml"
+command="yanglint -t nc-rpc -O ex-crypto-types-usage.xml ../ietf-crypto-types\@*.yang ../ex-crypto-types-usage\@*.yang ex-crypto-types-gcsr-rpc.xml"
 run_unix_cmd $LINENO "$command" 0
 printf "okay.\n"
 
 printf "Testing ex-crypto-types-gcsr-rpc-reply.xml..."
-command="yanglint -t reply ../ex-crypto-types-usage\@*.yang ex-crypto-types-gcsr-rpc-reply.xml ex-crypto-types-gcsr-rpc.xml"
+command="yanglint -t nc-reply -O ex-crypto-types-usage.xml -R ex-crypto-types-gcsr-rpc.xml ../ietf-crypto-types\@*.yang ../ex-crypto-types-usage\@*.yang ex-crypto-types-gcsr-rpc-reply.xml"
 run_unix_cmd $LINENO "$command" 0
 printf "okay.\n"
-
 
 printf "Testing ex-crypto-types-ce-notification.xml..."
 echo -e 'setns a=urn:ietf:params:xml:ns:netconf:notification:1.0\nsetns b=http://example.com/ns/example-crypto-types-usage\ncat //a:notification/b:asymmetric-keys' | xmllint --shell ex-crypto-types-ce-notification.xml | sed -e '/^\/.*/d' -e '/^ *$/d' > yanglint-notification.xml
-command="yanglint -t notif ex-crypto-types-usage.xml ../ex-crypto-types-usage\@*.yang  ../ietf-crypto-types\@20*.yang yanglint-notification.xml"
+command="yanglint -t notif -O ex-crypto-types-usage.xml ../ietf-crypto-types\@*.yang ../ex-crypto-types-usage\@*.yang yanglint-notification.xml"
 run_unix_cmd $LINENO "$command" 0
-printf "okay.\n"
 rm yanglint-notification.xml
+printf "okay.\n"
 
 
